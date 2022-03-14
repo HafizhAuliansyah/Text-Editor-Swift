70b0e5009301e4e5509473420008fe6e358fcd01
PanjiJF <panji.judha.tif21@polban.ac.id>
2022-03-14 12:38:02 +0700

    BenerinError

--- swift3.c
+++ swift3.c
@@ -176,7 +176,7 @@ int editorRowRxToCx (erow *row,int rx) {
 	int cx;
 	for (cx = 0; cx < row->size; cx++){
 		if (row->chars[cx] == '\t')
-			cur_rx += (KILO_TAB_STOP) - (cur_rx % KILO_TAB_STOP);
+			cur_rx += (SWIFT_TAB_STOP) - (cur_rx % SWIFT_TAB_STOP);
 		cur_rx++;
 		
 		if(cur_rx > rx) return cx;
